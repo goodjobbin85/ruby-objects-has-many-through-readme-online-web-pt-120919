3878b821afa49799f9759cbb5be0b5702dec0c65
thomas dubiel <td1729@hotmail.com>
2020-01-22 03:59:11 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,18 @@
 class Customer
+  attr_accessor :name, :age 
   
+  @@all = [] 
+  
+  def initialize(name, age) 
+    @name = name 
+    @age = age 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_meal(waiter, tip, total) 
+    Meal.new()
 end
